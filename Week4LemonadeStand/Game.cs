09fb2697333ff835e15048ec03d8944dc92b5f8a
DejaMeshae <daemeshae@yahoo.com>
2019-04-01 16:02:10 -0500

    initial commit

---
+++ Week4LemonadeStand/Game.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Week4LemonadeStand
+{
+    public class Game
+    {
+        /// <summary>
+        /// /
+        /// </summary>
+        private int Player2;
+
+        public UserInterface UserInterface
+        {
+            get => default(UserInterface);
+            set
+            {
+            }
+        }
+
+        public UserInterface UserInterface1
+        {
+            get => default(UserInterface);
+            set
+            {
+            }
+        }
+
+        public Player Player
+        {
+            get => default(Player);
+            set
+            {
+            }
+        }
+    }
+}
---
+++ Week4LemonadeStand/Store.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Week4LemonadeStand
+{
+    public class Store
+    {
+        private int inventory;
+
+        public Game Game
+        {
+            get => default(Game);
+            set
+            {
+            }
+        }
+    }
+}
